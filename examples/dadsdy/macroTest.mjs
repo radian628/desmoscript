50b93520447fc0885b57d463e7264d3bbf460456
radian628 <70352880+radian628@users.noreply.github.com>
2023-08-16 14:11:57 -0700

    cleaning up dumb code

--- examples/dadsdy/macroTest.mjs
+++ examples/dadsdy/macroTest.mjs
@@ -2,7 +2,6 @@ export default function ({ scope, addMacro }) {
   addMacro({
     name: "identity",
     fn: async (node, a) => {
-      // a.readFile("./a.txt");
       return a.node({
         type: "list",
         elements: new Array(10).fill(a.node(node.params[0])),
